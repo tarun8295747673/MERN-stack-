69198855b8a3fcb8b7311fd992922755f79bad5b
tarun8295747673 <tarunkumar2322806@gmail.com>
2025-01-19 17:59:01 +0530

    form validation

--- Day-16/Script/script.js
+++ Day-16/Script/script.js
@@ -4,30 +4,28 @@ function submitHandler(){
     const subject = getvalue('.subject');
     const message = getvalue('.message');
 
-console.log(name, email, subject, message);
+    console.log(name, email, subject, message);
 
-
-alert(name +"\n"+ email +"\n"+ subject +"\n"+ message);
-
-
+    alert(name +"\n"+ email +"\n"+ subject +"\n"+ message);
 }
 
 function getvalue(selector){
     return document.querySelector(selector).value;
 }
 
-
-function formValidation(){
+function formValidation(event){
+    event.preventDefault();
     const name = document.querySelector('.name').value;
     const email = document.querySelector('.email').value;
     const subject = document.querySelector('.subject').value;
     const message = document.querySelector('.message').value;
-    
+
+    let isValid = true;
 
     if(!name){
         const nameError = document.querySelector('#name-error');
         nameError.style.display = 'block';
-
+        isValid = false;
     }else{
         const nameError = document.querySelector('#name-error');
         nameError.style.display = 'none';
@@ -36,24 +34,31 @@ function formValidation(){
     if(!email){
         const emailError = document.querySelector('#email-error');
         emailError.style.display = 'block';
+        isValid = false;
     }else{
         const emailError = document.querySelector('#email-error');
         emailError.style.display = 'none';
     }
 
     if(!subject){
-        const subjectError = document.querySelector('#subject-error');
+        const subjectError = document.querySelector('#text-error');
         subjectError.style.display = 'block';   
+        isValid = false;
     }else{
-        const subjectError = document.querySelector('#subject-error');
+        const subjectError = document.querySelector('#text-error');
         subjectError.style.display = 'none';
     }    
 
     if(!message){
         const messageError = document.querySelector('#message-error');
         messageError.style.display = 'block';
+        isValid = false;
     }else{
         const messageError = document.querySelector('#message-error');
         messageError.style.display = 'none';   
     }
-    }
+
+    if (isValid) {
+        submitHandler();
+    }
+}
